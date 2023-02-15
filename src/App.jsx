452de035d747f51ec3a899e0dc60b6433c571cc7
Jeilanie <jeilanif@umich.edum>
2023-02-15 12:18:16 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+   <h1>To-DO</h1>
+   <h3>A task list that enables users to add their daily tasks</h3>
+   <button></button>
+    </div>
+  )
+}
+
+export default App
