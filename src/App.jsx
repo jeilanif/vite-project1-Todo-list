f770087ab9d5caaeb31511904fd3115eed9fcf40
Jeilanie <jeilanif@umich.edum>
2023-02-17 08:41:30 -0500

    App Interface
    completed html, css for App first page interface with button.
    
    Next Step: to connect the button to an even lister to show up the funcitonal "add" Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div id="main">
+      <img src="https://source.unsplash.com/VuwAfoHpxgs"></img>
     <div id="box">
   <div id="box-items">
    <h1 id="title">To-DO</h1>
