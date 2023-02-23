dd2f4ba7be0b2b2abcc4fedf5fbd121804e8e34f
Jeilanie <jeilanif@umich.edum>
2023-02-23 18:20:18 -0500

    Restrucure
    restructuring the Todo List
    
    Creating the form and the javascript first and focus on the Css for the main page and the project after.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div id="main">
-      <img src="https://source.unsplash.com/VuwAfoHpxgs"></img>
-    <div id="box">
-  <div id="box-items">
-   <h1 id="title">To-DO</h1>
-   <h3>A task list that enables users to add their daily tasks</h3>
-   <button>Get Started</button>
-   </div>
-    </div>
+    <div> 
+      Hello
     </div>
   )
+
 }
 
 export default App
